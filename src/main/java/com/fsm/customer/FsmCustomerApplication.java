ba482b0feb1f7e27f013f5a85d4c4ebf4401c5f1
siddharth2223437 <siddharthsarathe.nmims@gmail.com>
2021-09-23 23:02:25 -0500

    first commit

---
+++ src/main/java/com/fsm/customer/FsmCustomerApplication.java
@@ -0,0 +1,13 @@
+package com.fsm.customer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FsmCustomerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FsmCustomerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/fsm/customer/FsmCustomerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fsm.customer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FsmCustomerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
